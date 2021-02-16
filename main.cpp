358137ebf811cf68f26b0f685fe8fd6b24d015dc
grrishania <33783016+grrishania@users.noreply.github.com>
2021-02-16 11:43:43 +0300

    author reset

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    for (int i = 0 ; i < nums.size() - 1 ; i++) {
+      for (int k = i + 1 ; k < nums.size() ; k++) {
+        if (nums[i] + nums[k] == target) {
+          return {i, k};
+        }
+      }
+    }
+    return {};
+  }
+};
